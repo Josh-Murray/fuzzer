ad5b37b387f07ec77fa2b4e7922040797e5d1086
Gibble <joshua02murray@gmail.com>
2020-07-08 11:43:04 +1000

    added print functions for TestCase struct

--- test-case.go
+++ test-case.go
@@ -1,6 +1,23 @@
 package main
-
+import (
+  "fmt"
+)
 type TestCase struct {
   input []byte
   changes []string
+}
+
+func (ts TestCase) printTestCase() {
+ fmt.Println(string(ts.input))
+  for index, value := range ts.changes {
+    fmt.Printf("Change %d: %s", index, value)
+  }
+}
+func (ts TestCase) printInput() {
+ fmt.Println(string(ts.input))
+}
+func (ts TestCase) printChanges(){
+ for index, value := range ts.changes {
+    fmt.Printf("Change %d: %s", index, value)
+  }
 }
