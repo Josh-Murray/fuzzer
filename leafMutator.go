c4cf744d2e364c2664897a00f972e7bf8fbfdc1d
Clifford Sesel <z3217818@student.unsw.edu.au>
2020-07-09 12:22:46 +1000

    add initial mutator to detect and change a single integer

---
+++ leafMutator.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
+
+//func (m Mutator) swapSymbol(ts *TestCase) {
+//pick a symbol and swap it with a symbol of the same type
+//assume seperator is ' ' but might either detect or include what the seperator is in TestCase
+//	input := string(ts.input)
+//	result, msg := smartReplaceRandom(input)
+
+//	ts.changes = append(ts.changes, msg)
+//	ts.input = []byte(result)
+
+//}
+
+func smartReplace(word string) (string, string) {
+	var t, v string
+
+	if _, err := strconv.Atoi(word); err == nil {
+		t = "integer"
+		v = fmt.Sprintf("%v", rand.Uint32())
+	} else if _, err := strconv.ParseFloat(word, 1); err == nil {
+		t = "float"
+		v = fmt.Sprintf("%v", rand.ExpFloat64())
+	} else if _, err := strconv.ParseInt(word, 0, 64); err == nil {
+		t = "hex"
+		// TODO: check for octal
+		v = string("0xdeadbeef")
+	} else {
+		t = "word"
+		v = string("R4nd0m")
+	}
+
+	return v, t
+}
+
+func smartReplaceRandom(original string) (string, string) {
+	words := strings.Split(original, " ")
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	changeLocation := r1.Intn(len(words))
+
+	oldWord := words[changeLocation]
+	newWord, t := smartReplace(words[changeLocation])
+	words[changeLocation] = newWord
+	newString := strings.Join(words, " ")
+
+	fmt.Println("Original String:", original)
+	fmt.Println("Changing location:", changeLocation, "type:", t, "from:", oldWord, "to:", newWord)
+	fmt.Println("New String:", newString)
+
+	change := fmt.Sprintln("Changing location:", changeLocation, "type:", t, "from:", oldWord, "to:", newWord)
+
+	return newString, change
+}
+
+func interestingInt() int {
+	candidates := []int{0, -1, -100, 100}
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	return candidates[r1.Intn(len(candidates))]
+}
+
+func replaceRandInt(original string) (string, string) {
+	words := strings.Split(original, " ")
+	locations := make([]int, 0)
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	//find location of all integers in the input
+	for i, word := range words {
+		if _, err := strconv.Atoi(word); err == nil {
+			locations = append(locations, i)
+		}
+	}
+
+	changeLocation := locations[r1.Intn(len(locations))]
+	oldVal := words[changeLocation]
+	newVal := fmt.Sprintf("%v", interestingInt())
+	words[changeLocation] = newVal
+	change := fmt.Sprintln("replaceRandInt - location:", changeLocation, "from:", oldVal, "to: ", newVal)
+	newString := strings.Join(words, " ")
+	return newString, change
+}
+
+func main() {
+	var myInput string = "This -1 is 0x10f test -1.23 1090 input 1 10 1.30 with funn3y characters"
+	fmt.Println("Original String:", myInput)
+	//smartReplaceRandom(myInput)
+	result, msg := replaceRandInt(myInput)
+	fmt.Print(msg)
+	fmt.Println(result)
+}
