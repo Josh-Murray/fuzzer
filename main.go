747c6ff9f9279d545ee935b47ca0b57219541598
Gibble <joshua02murray@gmail.com>
2020-07-12 12:38:32 +1000

    Added seed param to mutator, added mutator and harness threads to main

--- main.go
+++ main.go
@@ -2,18 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 /*
  * Accepts an executable file and an input file to run it with.
  */
+
 func main() {
 	if len(os.Args) != 3 {
 		fmt.Println("Usage:", os.Args[0], "<binary>", "<input file>")
+		return
+	}
+	// create channels for mutator and harness
+	mutatorToHarness := make(chan TestCase)
+	harnessToInteresting := make(chan TestCase)
+
+	input, err := ioutil.ReadFile(os.Args[2])
+	if err != nil {
+		fmt.Println("Unable to read input file")
+		return
+	}
+	// create mutator threads
+	for i := 0; i < 4; i++ {
+		go func(i int) {
+			mutator := createMutator(mutatorToHarness, int64(i))
+			for {
+				temp := append([]byte{}, input...)
+				ts := &TestCase{temp, []string{}}
+				mutator.mutate(ts)
+			}
+		}(i)
+	}
+	// create harness threads
+	for i := 0; i < 4; i++ {
+		go harness(i, "./"+os.Args[1], mutatorToHarness, harnessToInteresting)
+
 	}
 
-	//binary := os.Args[1]
-	//inputFile := os.Args[2]
-
+	harness(4, "./"+os.Args[1], mutatorToHarness, harnessToInteresting)
 }
