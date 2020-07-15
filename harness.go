1674ac26e2a28ab253efb66baf72ba79e20e5902
seb <seb_costache@yahoo.com>
2020-07-15 13:32:15 +1000

    Convert harness to use ptrace
    Changes the harness to use ptrace and other syscalls to harness the
    binary from the old exec.Wait

--- harness.go
+++ harness.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	syscall "golang.org/x/sys/unix"
 	"log"
 	"os"
 	"os/exec"
-	"syscall"
+	"runtime"
 )
 
 /*
@@ -19,21 +20,34 @@ func harness(id int, cmd string,
 	for inputCase := range inputCases {
 		var err error
 		procCmd := exec.Command(cmd)
+		procCmd.SysProcAttr = &syscall.SysProcAttr{Ptrace: true}
 		procStdin, err := procCmd.StdinPipe()
 		if err != nil {
-			log.Fatalf("Harness with id %d failed to connect stdin pipe: %s",
+			log.Fatalf("Harness with id %d failed to connect stdin pipe: %s\n",
 				id, err.Error())
 		}
+
+		// Lock OS thread as per syscall.SysProcAttr documentation
+		runtime.LockOSThread()
 		err = procCmd.Start()
 		if err != nil {
-			log.Fatalf("Harness with id %d failed to start program: %s",
+			log.Fatalf("Harness with id %d failed to start program: %s\n",
 				id, err.Error())
 		}
 
 		procPid := procCmd.Process.Pid
+
+		// Child process recieves signal on startup.
+		var ws syscall.WaitStatus
+		_, err = syscall.Wait4(procPid, &ws, syscall.WALL, nil)
+		if err != nil {
+			log.Fatalf("Harness with id %d failed to wait: %s\n",
+				id, err.Error())
+		}
+
 		_, err = procStdin.Write(inputCase.input)
 		if err != nil {
-			log.Fatalf("Harness with id %d failed to write to program: %s",
+			log.Fatalf("Harness with id %d failed to write to program: %s\n",
 				id, err.Error())
 		}
 
@@ -44,11 +58,22 @@ func harness(id int, cmd string,
 				id)
 		}
 
-		procCmd.Wait()
+		err = syscall.PtraceCont(procPid, 0)
+		if err != nil {
+			log.Fatalf("Harness with id %d failed to make PtraceCont call: %s\n",
+				id, err.Error())
+		}
+
+		_, err = syscall.Wait4(procPid, &ws, syscall.WALL, nil)
+		if err != nil {
+			log.Fatalf("Harness with id %d failed to wait: %s\n",
+				id, err.Error())
+		}
+
+		runtime.UnlockOSThread()
 
 		// Report segfaults and ignore other exit causes.
-		waitStatus := procCmd.ProcessState.Sys().(syscall.WaitStatus)
-		if waitStatus.Signal() == syscall.SIGSEGV {
+		if ws.StopSignal() == syscall.SIGSEGV {
 			log.Printf("Harness with id %d crashed process with pid %d\n",
 				id, procPid)
 			crashReport(inputCase)
