b82918d9ec4e0f9e2ebcf4a064069699bd514b02
seb <seb_costache@yahoo.com>
2020-07-15 19:27:48 +1000

    Add initial feedback functionality
    This adds the ability for the harness to determine if a given input
    hit a new code path. It does this by using ptrace to track syscalls made
    by the harnessed program during a run, and comparing these traces across
    different executions.

--- harness.go
+++ harness.go
@@ -9,6 +9,46 @@ import (
 )
 
 /*
+ * Traps on every program syscall entry/exit and records the state.
+ * Function should be called after the child process has been started
+ * with ptrace enabled and has had syscall.Wait4 called on it once.
+ * Continues program until exit or crash: ws will be updated with the status.
+ * Takes as arguments the pid of process to trace and WaitStatus to update.
+ * Returns an execTrace struct identifying the execution run.
+ */
+func traceSyscalls(pid int, ws *syscall.WaitStatus) execTrace {
+	var err error
+	var regs syscall.PtraceRegs
+	var curExecTrace execTrace
+	for {
+		err = syscall.PtraceSyscall(pid, 0)
+		if err != nil {
+			log.Fatal("traceSyscalls failed to call PtraceSyscall")
+		}
+
+		_, err = syscall.Wait4(pid, ws, syscall.WALL, nil)
+		if err != nil {
+			log.Fatal("traceSyscalls failed to call Wait4")
+		}
+
+		// Return on program exit or crash.
+		if ws.Exited() == true || ws.StopSignal() == syscall.SIGSEGV {
+			return curExecTrace
+		}
+
+		// Collect trace information.
+		err = syscall.PtraceGetRegs(pid, &regs)
+		if err != nil {
+			log.Fatal("Getregs failed")
+			log.Fatal(err)
+		}
+
+		traceRegs := getInterestingRegs(&regs)
+		curExecTrace.trace = append(curExecTrace.trace, traceRegs)
+	}
+}
+
+/*
  * Harness will run the external binary specified by cmd and feed
  * it inputs from the inputCases channel. Interesting TestCases will
  * be placed in the interestCases output channel.
@@ -16,6 +56,9 @@ import (
 func harness(id int, cmd string,
 	inputCases <-chan TestCase,
 	interestCases chan<- TestCase) {
+
+	// List of unique execution traces for this harness.
+	var uniqueTraces []execTrace
 
 	for inputCase := range inputCases {
 		var err error
@@ -27,7 +70,7 @@ func harness(id int, cmd string,
 				id, err.Error())
 		}
 
-		// Lock OS thread as per syscall.SysProcAttr documentation
+		// Lock OS thread as per syscall.SysProcAttr documentation.
 		runtime.LockOSThread()
 		err = procCmd.Start()
 		if err != nil {
@@ -58,19 +101,13 @@ func harness(id int, cmd string,
 				id)
 		}
 
-		err = syscall.PtraceCont(procPid, 0)
-		if err != nil {
-			log.Fatalf("Harness with id %d failed to make PtraceCont call: %s\n",
-				id, err.Error())
-		}
-
-		_, err = syscall.Wait4(procPid, &ws, syscall.WALL, nil)
-		if err != nil {
-			log.Fatalf("Harness with id %d failed to wait: %s\n",
-				id, err.Error())
-		}
-
+		// Trace execution and report back interesting cases.
+		curExecTrace := traceSyscalls(procPid, &ws)
 		runtime.UnlockOSThread()
+		if isUniqueTrace(curExecTrace, uniqueTraces) {
+			uniqueTraces = append(uniqueTraces, curExecTrace)
+			interestCases <- inputCase
+		}
 
 		// Report segfaults and ignore other exit causes.
 		if ws.StopSignal() == syscall.SIGSEGV {
