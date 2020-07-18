95d9c5fa6f73ade292ac196866f043cb2aa3a20a
a-alexus-student <z3474352@ad.unsw.edu.au>
2020-07-18 12:46:49 +1000

    Add fileTypeAnalyser from filetypedetector feature branch
    Integrating the csv generator requires determining the input file type.
    filTypeAnalyser contains functions which detect whether or not a
    given file can be parsed as a csv file.

---
+++ fileTypeAnalyser.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+		panic(e)
+	}
+}
+
+func isValidXML(data []byte) bool {
+	err := xml.Unmarshal(data, new(interface{}))
+	if err == nil {
+		return true
+	}
+	return false
+	//return xml.Unmarshal(data, new(interface{})) != nil
+}
+
+func isValidCSV(file string) bool {
+	csvFile, _ := os.Open(file)
+	reader := csv.NewReader(csvFile)
+
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Println(err)
+			return false
+		}
+		fmt.Println(line, "with length", len(line))
+	}
+	return true
+}
+
+func detectType(file string) {
+
+	f, err := os.Open(file)
+	check(err)
+	f.Close()
+
+	data, err := ioutil.ReadFile(file)
+	check(err)
+	//fmt.Println(data)
+	ext := filepath.Ext(file)
+	fmt.Print("Extension is: ", ext)
+
+	if json.Valid(data) {
+		fmt.Print(" valid json")
+	} else {
+		fmt.Print(" invalid json")
+	}
+
+	if isValidXML(data) {
+		fmt.Print(" valid xml")
+	} else {
+		fmt.Print(" invalid xml")
+		fmt.Println(xml.Unmarshal(data, new(interface{})))
+	}
+
+	if isValidCSV(file) {
+		fmt.Print(" valid csv")
+	} else {
+		fmt.Print("invalid csv")
+	}
+}
+
+//func main() {
+//	detectType("mutator.go")
+//}
